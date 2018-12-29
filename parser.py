9b82fd8be281b2b33de4ad3e05c449b61784e1e7
pchan <prasad.chand@gmail.com>
2018-12-29 13:34:40 +0530

    Add argument parser to the base class. Add a new gsft func that returns the token val as str.

--- parser.py
+++ parser.py
@@ -4,6 +4,7 @@
 # It initializes the lexer, defines the input string that make up
 import logging
 import sys
+import argparse
 #Please see README on how to get this lexer
 import lexer
 
@@ -17,8 +18,6 @@ class Parser(object):
         self.lx = lexer.Lexer(rules, skip_whitespace=True)
         self.cur_token = None
         self.next_token = None
-        # dictionary for variables (e.g. set x  = 10)
-        self.vars = {}
 
     #dump token status (including caller info)
     def dts(self, caller=sys._getframe().f_code.co_name):
@@ -56,27 +55,50 @@ class Parser(object):
             return None
         return int(token.val)
 
+    #get val(str) from token
+    def gsft(self, token=None):
+        if token is None:
+            token = self.cur_token
+        if token is None:
+            logging.critical("token not present")
+            return None
+        return str(token.val)
+
     def match(self, type):
         if (self.next_token and self.next_token.type == type):
             logging.info("Parsing token {}".format(self.next_token))
             self.uct(self.next_token)
             self.gnt()
-            self.dts(sys._getframe().f_code.co_name)
+            #self.dts(sys._getframe().f_code.co_name)
             return True
         return False
 
     #derived classes NEED to implement this function
     def start(self):
-        self.dts(sys._getframe().f_code.co_name)
         raise NotImplementedError()
 
-    #parse 
+    #parse the input
     def parse(self, input="(5+6)"):
         logging.debug("input is {}".format(input))
         self.lx.input(input)
         success, result = self.start()
+
         if success:
             print("Success! Result is {}".format(result))
         else:
             print("Parse Error")
+
+    #parse the argument input
+    def parse_arguments(self):
+        """ deal with all the options being passed in"""
+
+        arg_parser = argparse.ArgumentParser(description="Parser arguments")
+        arg_parser.add_argument('--input', dest='input', action='store', default="5", type=str, help='Specify input to parser')
+        arg_parser.add_argument("-v", "--verbose", help="increase output verbosity(debug)",
+                action="store_true")
+        arg_parser.add_argument("-t", "--trace", help="trace the execution",
+                action="store_true")
+        args = arg_parser.parse_args()
+
+        return args
 
